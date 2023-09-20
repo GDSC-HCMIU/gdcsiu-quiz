eb40530110b425615ffed60265cc8234afb79d6a
TrNgTien <ngoctien27122001@gmail.com>
2023-09-20 21:10:31 +0700

    Tien | Fix | fix convention code for timer component

--- client/src/components/Quiz/Timer.jsx
+++ client/src/components/Quiz/Timer.jsx
@@ -3,32 +3,36 @@ import React, { useState, useEffect } from 'react';
 const Timer = ({ initialTime, onTimeExpired }) => {
   const [seconds, setSeconds] = useState(initialTime);
   const [isActive, setIsActive] = useState(true);
+  let interval;
 
   useEffect(() => {
-    let interval;
 
-    if (isActive && seconds > 0) {
-      interval = setInterval(() => {
-        setSeconds((prevSeconds) => prevSeconds - 1);
-      }, 1000);
-    } else if (seconds === 0) {
+    if (!seconds) {
       setIsActive(false);
       onTimeExpired();
       clearInterval(interval);
     }
 
-    return () => clearInterval(interval);
+    interval = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval)
+    };
+
   }, [isActive, seconds, onTimeExpired]);
 
-  const formatTime = (time) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-  };
+  const formatTime = React.useMemo(() => {
+    const minutes = Math.floor(seconds / 60);
+    const secondLeft = (seconds % 60);
+
+    return `${minutes}:${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`;
+  }, [seconds]);
 
   return (
     <div className="flex-col font-bold text-2xl mb-4 text-start">
-      {formatTime(seconds)}
+      {formatTime}
     </div>
   );
 };
